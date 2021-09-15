56901292b8053438112506268e5bd50ba5538074
alfer91 <dasungwkd@naver.com>
2021-09-15 11:14:59 +0900

    게시물 수정 처리

--- src/test/java/org/zerock/board/service/BoardServiceTest.java
+++ src/test/java/org/zerock/board/service/BoardServiceTest.java
@@ -49,11 +49,23 @@ class BoardServiceTest {
         System.out.println(boardDTO);
     }*/
 
-    @Test
+    /*@Test
     public void testRemove() {
 
         Long bno = 1L;
 
         boardService.removeWithReplies(bno);
+    }*/
+
+    @Test
+    public void testModify() {
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(2L)
+                .title("제목 변경합니다.")
+                .content("내용 변경합니다.")
+                .build();
+
+        boardService.modify(boardDTO);
     }
 }
